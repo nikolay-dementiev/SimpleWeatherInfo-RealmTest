df0e7afdf0030260666edce675401a28fdc7316e
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-07-27 16:45:50 +0300

    first

---
+++ SimpleWeatherInfo-RealmTest/SimpleWeatherInfo-RealmTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleWeatherInfo-RealmTest
+//
+//  Created by Nikolay Dementiev on 27.07.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
